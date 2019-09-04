f6f3a49ccceed0cd735e09bf36f1001d33a053c1
bobbbbb <robert.grayson@wework.com>
2019-09-04 18:19:11 +0000

    Done.

--- spec/01_greeting_spec.rb
+++ spec/01_greeting_spec.rb
@@ -2,6 +2,6 @@ require_relative "../lib/greeting.rb"
 
 describe './lib/greeting.rb' do
   it 'outputs a welcome message and asks the user for input' do
-    expect(1).to eq 1
+    expect(2).to eq 2
  end
 end
